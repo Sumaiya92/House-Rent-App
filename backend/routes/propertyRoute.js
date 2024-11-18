fd54694aff39757e6f5b238404c2711c40ce0c91
Sumaiya92 <111109084+Sumaiya92@users.noreply.github.com>
2024-11-18 10:25:40 +0530

    Delete backend directory

--- backend/routes/propertyRoute.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import {getPropertyDetails} from '../controllers/propertyController.js';
-const router = express.Router();
-
-router.get('/:id',getPropertyDetails);
-export default router;
